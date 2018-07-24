f8bfdfc425888fa0ba48069e62a3055630078ce7
Tran Thu Hue <94huetran@gmail.com>
2018-07-24 15:36:27 +0700

    update SizeFile

--- src/main/java/File/SizeFile.java
+++ src/main/java/File/SizeFile.java
@@ -4,21 +4,21 @@ import java.io.File;
 import java.util.Scanner;
 
 public class SizeFile {
-    public static long getSize(File file) throws Exception {
-        File[] list = file.listFiles();
-        long size = 0;
-        for (File f: list) {
-            size = size + f.length();
-        }
-        return size;
-    }
-
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         System.out.println("Input path file: ");
         String path = scn.nextLine();
         File file = new File(path);
         System.out.println("Size of this file is: " + getSize(file));
+    }
 
+     public static long getSize(File file) throws Exception {
+        File[] list = file.listFiles();
+        long size = 0;
+        for (File f : list) {
+            if (f.isFile())  size += f.length();
+            else if (f.isDirectory()) size += getSize(f);
+        }
+        return size;
     }
 }
